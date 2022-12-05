3441a6296b278231d1610b6b69397d77b7d5bba1
Shadow032020 <72571768+Shadow032020@users.noreply.github.com>
2022-12-05 15:21:13 +0100

    Update EmployeeFactory.java

--- src/fr/efrei/factory/EmployeeFactory.java
+++ src/fr/efrei/factory/EmployeeFactory.java
@@ -4,35 +4,24 @@ import fr.efrei.util.Helper;
 //Done
 
 public class EmployeeFactory {
-    public static Employee createEmployee(int employeeID, String employeeGender, String employeeRace){
-        if (Helper.isNullOrEmpty(employeeID) || Helper.isNullOrEmpty(employeeGender) ||Helper.isNullOrEmpty(employeeRace)){
+    public static Employee createEmployee(String employeeGender, String employeeRace){
+        String employeeID = Helper.generateId();
+        if (Helper.isNullOrEmpty(employeeGender) ||Helper.isNullOrEmpty(employeeRace)){
             return null;
         }
-        Employee employee = new Employee.Builder().setEmployeeID(employeeID)
+        Employee employee = new Employee.Builder().setEmployeeID(Integer.valueOf(employeeID))
                 .setEmployeeGender(employeeGender)
                 .setEmployeeRace(employeeRace)
                 .build();
         return employee;
     }
     
-    public static Employee createEmployee(String employeeGender, String employeeRace){
-        if ( Helper.isNullOrEmpty(employeeGender) ||Helper.isNullOrEmpty(employeeRace)){
+    public static Employee createEmployee(String employeeRace){
+        if (Helper.isNullOrEmpty(employeeRace)){
             return null;
         }
-        Employee employee = new Employee.Builder().setEmployeeGender(employeeGender)
-                .setEmployeeRace(employeeRace)
+        Employee employee = new Employee.Builder().setEmployeeRace(employeeRace)
                 .build();
         return employee;
     }
-    
-    
-    public static Employee createEmployee(int employeeID){
-        if (Helper.isNullOrEmpty(employeeID)){
-            return null;
-        }
-        Employee employee = new Employee.Builder().setEmployeeID(employeeID)
-                .build();
-        return employee;
-    }
-    
 }
