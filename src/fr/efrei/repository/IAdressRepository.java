264bfd64faf0253aaa0c7306011d6cc614173620
Wilthien <kaspar.wilthien@gmail.com>
2022-12-05 12:13:25 +0100

    Update

--- src/fr/efrei/repository/IAdressRepository.java
+++ src/fr/efrei/repository/IAdressRepository.java
@@ -3,7 +3,7 @@ import fr.efrei.domain.Address;
 import java.util.List;
 
 
-public interface IAdressRepository extends IRepository<Adress, String>{
+public interface IAdressRepository extends IRepository<Address, String> {
     //create, read, update, delete: override these 4 methods
     public List<Address> getAll();
 }
