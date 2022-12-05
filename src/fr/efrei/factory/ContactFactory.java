2a724f653767343d02d581fdea8fab564b722fcd
GaboNueve <94602569+GaboNueve@users.noreply.github.com>
2022-12-05 13:11:04 +0100

    Update ContactFactory.java

--- src/fr/efrei/factory/ContactFactory.java
+++ src/fr/efrei/factory/ContactFactory.java
@@ -15,4 +15,23 @@ public class ContactFactory {
                 .build();
         return contact;
     }
+     public static Contact createContact (int cellPhoneNumber, String email){
+        if (Helper.isNullOrEmpty(cellPhoneNumber) || Helper.isNullOrEmpty(email)){
+            return null;
+        }
+        Contact contact = new Contact.Builder().setcellPhoneNumber(cellPhoneNumber)
+                .setemail(email)
+                .build();
+        return contact;
+    }
+
+    public static Contact createContact (int homeNumber){
+        if ( Helper.isNullOrEmpty(homeNumber)){
+            return null;
+        }
+        Contact contact = new Contact.Builder().sethomeNumber(homeNumber)
+                .build();
+        return contact;
+    }
 }
+
