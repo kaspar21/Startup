a0666ce24146f19db5592fd2d8b9d3dab258ecb2
Wilthien <kaspar.wilthien@gmail.com>
2022-12-05 12:24:05 +0100

    Update

--- src/fr/efrei/repository/AdressRepository.java
+++ src/fr/efrei/repository/AdressRepository.java
@@ -1,4 +1,30 @@
 package fr.efrei.repository;
+import fr.efrei.domain.Address;
+import java.util.ArrayList;
+import java.util.List;
 
-public class AdressRepository {
+public class AdressRepository implements IAdressRepository{
+    private static AdressRepository repository = null;
+    private List<Adress> adressDB = null;
+
+    private AdressRepository(){adressDB = new ArrayList<Adress>();}
+
+    public static AdressRepository getRepository() {
+        if (repository == null){
+            return new AdressRepository();
+        }
+        return repository;
+    }
+    @Override
+    public Adress create(Adress adress){
+        boolean success = adressDB.add(adress);
+        if (success){
+            return adress;
+    }
+        else {
+            return null;
+        }
+    }
+
+
 }
