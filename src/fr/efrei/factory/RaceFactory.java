c3e8d7a0f12d3e2afd8759de5751d5cf43c9e03e
Wilthien <kaspar.wilthien@gmail.com>
2022-12-05 13:44:44 +0100

    Update

--- src/fr/efrei/factory/RaceFactory.java
+++ src/fr/efrei/factory/RaceFactory.java
@@ -5,15 +5,12 @@ import fr.efrei.util.Helper;
 public class RaceFactory {
     public static Race createRace(String description){
         String id = Helper.generateId();
-
         if (Helper.isNullOrEmpty(description)){
             return null;
         }
-
         Race race = new Race.Builder().setId(id) //referring to the BUILDER class inside the Race class
                 .setDescription(description)
                 .build();
-
         return race;
     }
 
