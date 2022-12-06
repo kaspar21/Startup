bcaecc3bfd31be17b8ab705d211b0583296f3338
Wilthien <kaspar.wilthien@gmail.com>
2022-12-06 22:48:06 +0100

    Kaspar's update

--- src/fr/efrei/factory/EmployeeFactory.java
+++ src/fr/efrei/factory/EmployeeFactory.java
@@ -1,12 +1,26 @@
 package fr.efrei.factory;
-import fr.efrei.domain.Employee;
+import fr.efrei.domain.*;
 import fr.efrei.util.Helper;
-//Done
 
 public class EmployeeFactory {
-    public static Employee createEmployee(String employeeGender, String employeeRace){
+    //Tous les attributs
+    public static Employee createEmployee(int employeeID, Gender employeeGender, Race employeeRace, double salary, Address employeeAdress, Contact employeeContact){
+        if (Helper.isNullOrEmpty(employeeID) || employeeGender == null  || employeeRace == null || Helper.isNullOrEmpty(salary) || employeeAdress == null || employeeContact == null ){
+            return null;
+        }
+        Employee employee = new Employee.Builder().setEmployeeID(employeeID)
+                .setEmployeeGender(employeeGender)
+                .setEmployeeRace(employeeRace)
+                .setSalary(salary)
+                .setEmployeeAdress(employeeAdress)
+                .setEmployeeContact(employeeContact)
+                .build();
+        return employee;
+    }
+    //Que gender et race
+    public static Employee createEmployee(Gender employeeGender, Race employeeRace){
         String employeeID = Helper.generateId();
-        if (Helper.isNullOrEmpty(employeeGender) ||Helper.isNullOrEmpty(employeeRace)){
+        if (employeeGender == null  || employeeRace == null){
             return null;
         }
         Employee employee = new Employee.Builder().setEmployeeID(Integer.valueOf(employeeID))
@@ -15,9 +29,9 @@ public class EmployeeFactory {
                 .build();
         return employee;
     }
-    
-    public static Employee createEmployee(String employeeRace){
-        if (Helper.isNullOrEmpty(employeeRace)){
+
+    public static Employee createEmployee(Race employeeRace){
+        if (employeeRace == null){
             return null;
         }
         Employee employee = new Employee.Builder().setEmployeeRace(employeeRace)
