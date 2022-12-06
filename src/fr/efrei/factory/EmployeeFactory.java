464151f7810627dc4883d1d0436f24a07a98f336
Wilthien <kaspar.wilthien@gmail.com>
2022-12-06 22:44:55 +0100

    Kaspar's update

--- src/fr/efrei/factory/EmployeeFactory.java
+++ src/fr/efrei/factory/EmployeeFactory.java
@@ -1,10 +1,23 @@
 package fr.efrei.factory;
-import fr.efrei.domain.Employee;
-import fr.efrei.domain.Gender;
-import fr.efrei.domain.Race;
+import fr.efrei.domain.*;
 import fr.efrei.util.Helper;
 
 public class EmployeeFactory {
+    //Tous les attributs
+    public static Employee createEmployee(int employeeID, Gender employeeGender, Race employeeRace, double salary, Address employeeAdress, Contact employeeContact){
+        if (Helper.isNullOrEmpty(employeeID) || employeeGender == null  || employeeRace == null || Helper.isNullOrEmpty(salary) || employeeAdress == null || employeeContact == null ){
+            return null;
+        }
+        Employee employee = new Employee.Builder().setEmployeeID(employeeID)
+                .setEmployeeGender(employeeGender)
+                .setEmployeeRace(employeeRace)
+                .setSalary(salary)
+                .setEmployeeAdress(employeeAdress)
+                .setEmployeeContact(employeeContact)
+                .build();
+        return employee;
+    }
+    //Que gender et race
     public static Employee createEmployee(Gender employeeGender, Race employeeRace){
         String employeeID = Helper.generateId();
         if (employeeGender == null  || employeeRace == null){
