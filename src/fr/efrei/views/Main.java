96602a5a752ddbf4f1bea8f4253e7b7b0616fb43
Wilthien <kaspar.wilthien@gmail.com>
2022-12-06 23:07:16 +0100

    Kaspar's update

--- src/fr/efrei/views/Main.java
+++ src/fr/efrei/views/Main.java
@@ -6,10 +6,8 @@ import fr.efrei.util.Helper;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         //Employee employee = new Employee.EmployeeBuilder("Vikram", "ABC").setHasBike(false).setHasBike(true).build();
-
         System.out.println("Enter a number to select a command : /n");
         System.out.println("0. Create an employee /n");
         System.out.println("1. Create and add an employee to the list /n");
@@ -20,25 +18,18 @@ public class Main {
         Scanner in = new Scanner(System.in);
         int number=in.nextInt();
         while (number!=5){
-
             switch(number) {
                 case 0:
-
                     break;
                 case 1:
-
                     break;
                 case 2:
-
                     break;
                 case 3:
-
                     break;
                 case 4:
-
                     break;
                 case 5:
-
                     break;
                 default:
                     System.out.println("Enter a number to select a command : /n");
