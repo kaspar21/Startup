e78acdc70b2d155205d041f03182ef8e9aa73683
GaboNueve <gabriel.de.chambost@efrei.net>
2022-12-06 21:45:32 +0100

    GaboNueve's Update

--- src/fr/efrei/domain/Gender.java
+++ src/fr/efrei/domain/Gender.java
@@ -2,7 +2,6 @@ package fr.efrei.domain;
 //Done
 
 public class Gender {
-    private String genderID;
     private String genderDescription;
 
     //default constructor
@@ -10,14 +9,11 @@ public class Gender {
 
     //add another constructor later
     private Gender(Builder builder){
-        this.genderID = builder.genderID;
         this.genderDescription = builder.genderDescription;
     }
 
     //getters but no setters, to have protected classes
-    public String getgenderID() {
-        return genderID;
-    }
+
 
     public String getgenderDescription() {
         return genderDescription;
@@ -26,21 +22,17 @@ public class Gender {
     @Override
     public String toString() {
         return "Gender{" +
-                "genderID='" + genderID + '\'' +
                 ", genderDescription='" + genderDescription + '\'' +
                 '}';
     }
 
     //builder class = static inner class, copy the same attributes at the top
     public static class Builder {
-        private String genderID;
+
         private String genderDescription;
 
         //setters, not getters
-        public Builder setGenderID(String genderID) {
-            this.genderID = genderID;
-            return this; //return the Builder object
-        }
+
 
         public Builder setgenderDescription(String genderDescription) {
             this.genderDescription = genderDescription;
@@ -48,7 +40,6 @@ public class Gender {
         }
 
         public Builder copy(Gender gender){
-            this.genderID = gender.genderID;
             this.genderDescription = gender.genderDescription;
             return this; //return the Builder object
         }
--- src/fr/efrei/repository/GenderRepository.java
+++ src/fr/efrei/repository/GenderRepository.java
@@ -1,4 +1,73 @@
+
 package fr.efrei.repository;
 
-public class GenderRepository {
+import fr.efrei.domain.Gender;
+
+import java.util.ArrayList;
+import java.util.List;
+
+    public class GenderRepository implements IGenderRepository {
+        private static fr.efrei.repository.GenderRepository repository = null;
+        private List<Gender> genderDB = null;
+
+        private GenderRepository() {
+            genderDB = new ArrayList<Gender>();
+        }
+
+        public static fr.efrei.repository.GenderRepository getRepository() {
+            if (repository == null){
+                return new fr.efrei.repository.GenderRepository();
+            }
+            return repository;
+        }
+
+
+        @Override
+        public Gender create(Gender gender) {
+            boolean success = genderDB.add(gender); //indeed, add method returns a boolean
+            if (success){
+                return gender;
+            }
+            else {
+                return null;
+            }
+        }
+
+
+        @Override
+        public Gender read(String s) {
+            for (Gender i : genderDB){
+                if (i.getgenderDescription().equals(s)){
+                    return i;
+                }
+
+            }
+            return null; //return null if we don't find the object in the list
+        }
+    @Override
+    public Gender update(Gender gender) {
+        Gender oldGender = read(gender.getgenderDescription());
+        if (gender != null){
+            genderDB.remove(oldGender); //take the object away from the data structure
+            genderDB.add(gender);
+            return gender;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean delete(String s) {
+        Gender genderToDelete = read(s);
+        if (genderToDelete == null) {
+            return false;
+        }
+        genderDB.remove(genderToDelete);
+        return true;
+    }
+
+    @Override
+    public List<Gender> getAll() {
+        return genderDB;
+    }
+
 }
