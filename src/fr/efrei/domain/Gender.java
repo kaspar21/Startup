1009c805bbac5e9e6d2830e2aac3f3ce0f2a90b1
GaboNueve <94602569+GaboNueve@users.noreply.github.com>
2022-12-06 11:13:42 +0100

    Create Gender.java

---
+++ src/fr/efrei/domain/Gender.java
@@ -0,0 +1,62 @@
+package fr.efrei.domain;
+//Done
+
+public class Gender {
+    private String genderID;
+    private String genderDescription;
+
+    //default constructor
+    private Gender() {}
+
+    //add another constructor later
+    private Gender(Builder builder){
+        this.genderID = builder.genderID;
+        this.genderDescription = builder.genderDescription;
+    }
+
+    //getters but no setters, to have protected classes
+    public String getgenderID() {
+        return genderID;
+    }
+
+    public String getgenderDescription() {
+        return genderDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "Gender{" +
+                "genderID='" + genderID + '\'' +
+                ", genderDescription='" + genderDescription + '\'' +
+                '}';
+    }
+
+    //builder class = static inner class, copy the same attributes at the top
+    public static class Builder {
+        private String genderID;
+        private String genderDescription;
+
+        //setters, not getters
+        public Builder setId(String genderID) {
+            this.genderID = genderID;
+            return this; //return the Builder object
+        }
+
+        public Builder setgenderDescription(String genderDescription) {
+            this.genderDescription = genderDescription;
+            return this; //return the Builder object
+        }
+
+        public Builder copy(Gender gender){
+            this.genderID = race.genderID;
+            this.genderDescription = race.genderDescription;
+            return this; //return the Builder object
+        }
+
+        //build a race object -- like copy constructor in C++
+        public Gender build() {
+            return new Gender(this);
+        }
+    }
+
+}
