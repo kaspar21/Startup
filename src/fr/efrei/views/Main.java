c3d458d659d997d2d0e0084e956dc42b0ba798dc
Wilthien <kaspar.wilthien@gmail.com>
2022-12-06 23:05:24 +0100

    Kaspar's update

--- src/fr/efrei/views/Main.java
+++ src/fr/efrei/views/Main.java
@@ -1,4 +1,11 @@
 package fr.efrei.views;
 
 public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        //domain = put all the classes and entities
+        //factory = objects that you create for your 'entities
+        //repository = where you keep your data stored
+        //util = helper classes as if an email @ is valid
+    }
 }
