7cd58ec36eaff2976fb4448e75a87649b3dd7a07
GaboNueve <gabriel.de.chambost@efrei.net>
2022-12-06 19:49:15 +0100

    GaboNueve's Update

--- src/fr/efrei/domain/Job.java
+++ src/fr/efrei/domain/Job.java
@@ -4,8 +4,59 @@ public class Job {
     private int positionCode;
     private String title;
 
-    public Job(int positionCode, String title) {
-        this.positionCode = positionCode;
-        this.title = title;
+    //default constructor
+    private Job() {}
+
+    //add another constructor later
+    private Job(Builder builder){
+        this.positionCode = builder.positionCode;
+        this.title = builder.title;
     }
+
+    //getters but no setters, to have protected classes
+    public int getpositionCode() {
+        return positionCode;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "Job{" +
+                "positionCode='" + positionCode + '\'' +
+                ", title='" + title + '\'' +
+                '}';
+    }
+
+    //builder class = static inner class, copy the same attributes at the top
+    public static class Builder {
+        private int positionCode;
+        private String title;
+
+        //setters, not getters
+        public Builder setpositionCode(int positionCode) {
+            this.positionCode = positionCode;
+            return this; //return the Builder object
+        }
+
+        public Builder settitle(String title) {
+            this.title = title;
+            return this; //return the Builder object
+        }
+
+        public Builder copy(Job job){
+            this.positionCode = job.positionCode;
+            this.title = job.title;
+            return this; //return the Builder object
+        }
+
+        //build a job object -- like copy constructor in C++
+        public Job build() {
+            return new Job(this);
+        }
+    }
+
 }
+
